7731f3cf3fed65f056cd60afea3991ff4d15ca0c
Taylor Romero <taylorromero@beccasmacbookpro.local>
2020-08-09 19:36:43 -0600

    patch: types and tests started

---
+++ packages/mercury-types/src/TestClient.ts
@@ -0,0 +1,24 @@
+
+
+import MercuryClient, { MercuryContract, ExtractListenerPayload } from "Mercury.types";
+import { SchemaValues } from "@sprucelabs/schema";
+
+
+interface TestContract extends MercuryContract {
+    spruce: {
+        test: {
+
+        }
+    }
+}
+
+
+export class TestClient implements MercuryClient<TestContract> {
+    public async on<EventNamespace extends string, EventName extends string, ListenPayload extends SchemaValues<TestContract[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<TestContract[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, cb?: (payload: ListenPayload) => ListenResponsePayload | Promise<ListenResponsePayload>): Promise<void> {
+
+    }
+
+    public async emit<EventNamespace extends string, EventName extends string, EmitPayload extends SchemaValues<TestContract[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<TestContract[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, payload: EmitPayload, cb?: (payload: ListenResponsePayload) => void | Promise<void>): Promise<void> {
+
+    }
+}
