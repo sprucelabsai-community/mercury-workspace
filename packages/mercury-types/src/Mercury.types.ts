4d3c6aac6aef294ce89de056cc90a6b65c09b044
Ryan Johnson <ryan@sprucelabs.ai>
2020-08-10 00:07:02 -0400

    chore: formatting

--- packages/mercury-types/src/Mercury.types.ts
+++ packages/mercury-types/src/Mercury.types.ts
@@ -1,25 +1,60 @@
 import { ISchema, SchemaValues } from '@sprucelabs/schema'
 
 export interface MercuryResponse {
-    payload: ISchema
+	payload: ISchema
 }
 
 export interface MercuryContract {
-    [eventNamespace: string]: {
-        [eventName: string]: {
-            listenResponsePayload: ISchema | MercuryResponse
-            emitPayload: ISchema
-        }
-    }
+	[eventNamespace: string]: {
+		[eventName: string]: {
+			listenResponsePayload: ISchema | MercuryResponse
+			emitPayload: ISchema
+		}
+	}
 }
 
-export type ExtractListenerPayload<Payload extends ISchema | MercuryResponse> = Payload extends ISchema ? ISchema : MercuryResponse['payload']
+export type ExtractListenerPayload<
+	Payload extends ISchema | MercuryResponse
+> = Payload extends ISchema ? ISchema : MercuryResponse['payload']
 
 export default interface MercuryClient<C extends MercuryContract> {
-    on<EventNamespace extends string, EventName extends string, ListenPayload extends SchemaValues<C[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<C[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, cb?: (payload: ListenPayload) => ListenResponsePayload | Promise<ListenResponsePayload>): Promise<void>
-    emit<EventNamespace extends string, EventName extends string, EmitPayload extends SchemaValues<C[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<C[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, payload: EmitPayload, cb?: (payload: ListenResponsePayload) => void | Promise<void>): Promise<void>
+	on<
+		EventNamespace extends string,
+		EventName extends string,
+		ListenPayload extends SchemaValues<
+			C[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				C[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		cb?: (
+			payload: ListenPayload
+		) => ListenResponsePayload | Promise<ListenResponsePayload>
+	): Promise<void>
+	emit<
+		EventNamespace extends string,
+		EventName extends string,
+		EmitPayload extends SchemaValues<
+			C[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				C[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		payload: EmitPayload,
+		cb?: (payload: ListenResponsePayload) => void | Promise<void>
+	): Promise<void>
 }
 
 export interface MercuryConstructor<C extends MercuryContract> {
-    (): MercuryClient<C>
+	(): MercuryClient<C>
 }
--- packages/mercury-types/src/__tests__/behavioral/TypeMappingRocks.ts
+++ packages/mercury-types/src/__tests__/behavioral/TypeMappingRocks.ts
@@ -1,15 +1,13 @@
 import AbstractSpruceTest, { test, assert } from '@sprucelabs/test'
 
 export default class TypesWorkTest extends AbstractSpruceTest {
-
 	@test()
 	protected static async firstTest() {
 		// test here
 	}
 
-
 	@test()
 	protected static async typesWork() {
-		assert.isTrue(false)	
+		assert.isTrue(false)
 	}
 }
