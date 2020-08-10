2a6b8e54ceb9bd2393bcd41d8e3d9c9c073f3719
Taylor Romero <taylorromero@beccasmacbookpro.local>
2020-08-10 08:00:34 -0600

    patch: lint.fix

--- packages/mercury-types/src/Mercury.types.ts
+++ packages/mercury-types/src/Mercury.types.ts
@@ -1,49 +1,65 @@
 import { ISchema, SchemaValues } from '@sprucelabs/schema'
 
 export interface MercuryPagedResponse {
-    payload: ISchema
+	payload: ISchema
 }
 
 export interface MercuryAggregateResults {
-    responses: {}
+	responses: {}
 }
 
 export interface MercuryContract {
-    [eventNamespace: string]: {
-        [eventName: string]: {
-            listenResponsePayload: ISchema | MercuryPagedResponse
-            emitPayload: ISchema
-        }
-    }
+	[eventNamespace: string]: {
+		[eventName: string]: {
+			listenResponsePayload: ISchema | MercuryPagedResponse
+			emitPayload: ISchema
+		}
+	}
 }
 
-export type ExtractListenerPayload<Payload extends ISchema | MercuryPagedResponse> = Payload extends ISchema ? ISchema : MercuryPagedResponse['payload']
+export type ExtractListenerPayload<
+	Payload extends ISchema | MercuryPagedResponse
+> = Payload extends ISchema ? ISchema : MercuryPagedResponse['payload']
 
 export default interface MercuryClient<C extends MercuryContract> {
-    on<
-        EventNamespace extends string, 
-        EventName extends string, 
-        EmitPayload extends SchemaValues<C[EventNamespace][EventName]['emitPayload']>, 
-        ListenResponsePayload extends SchemaValues<ExtractListenerPayload<C[EventNamespace][EventName]['listenResponsePayload']>>
-    >(
-        eventNamespace: EventNamespace, 
-        eventName: EventName, 
-        cb?: (payload: EmitPayload) => ListenResponsePayload | Promise<ListenResponsePayload>
-    ): Promise<void>
+	on<
+		EventNamespace extends string,
+		EventName extends string,
+		EmitPayload extends SchemaValues<
+			C[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				C[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		cb?: (
+			payload: EmitPayload
+		) => ListenResponsePayload | Promise<ListenResponsePayload>
+	): Promise<void>
 
-    emit<
-        EventNamespace extends string, 
-        EventName extends string, 
-        EmitPayload extends SchemaValues<C[EventNamespace][EventName]['emitPayload']>, 
-        ListenResponsePayload extends SchemaValues<ExtractListenerPayload<C[EventNamespace][EventName]['listenResponsePayload']>>
-    >(
-        eventNamespace: EventNamespace, 
-        eventName: EventName, 
-        payload: EmitPayload, 
-        cb?: (payload: ListenResponsePayload) => void | Promise<void>
-    ): Promise<void>
+	emit<
+		EventNamespace extends string,
+		EventName extends string,
+		EmitPayload extends SchemaValues<
+			C[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				C[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		payload: EmitPayload,
+		cb?: (payload: ListenResponsePayload) => void | Promise<void>
+	): Promise<void>
 }
 
 export interface MercuryConstructor<C extends MercuryContract> {
-    (): MercuryClient<C>
+	(): MercuryClient<C>
 }
--- packages/mercury-types/src/TestClient.ts
+++ packages/mercury-types/src/TestClient.ts
@@ -1,25 +1,53 @@
-
-
-import MercuryClient, { MercuryContract, ExtractListenerPayload } from "Mercury.types";
-import { SchemaValues } from "@sprucelabs/schema";
-
+import { SchemaValues } from '@sprucelabs/schema'
+import MercuryClient, {
+	MercuryContract,
+	ExtractListenerPayload,
+} from 'Mercury.types'
 
 interface TestContract extends MercuryContract {
-    spruce: {
-        test: {
-            listenResponsePayload: {}
-            emitPayload:{}
-        }
-    }
+	spruce: {
+		test: {
+			listenResponsePayload: {}
+			emitPayload: {}
+		}
+	}
 }
 
-
 export class TestClient implements MercuryClient<TestContract> {
-    public async on<EventNamespace extends string, EventName extends string, ListenPayload extends SchemaValues<TestContract[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<TestContract[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, cb?: (payload: ListenPayload) => ListenResponsePayload | Promise<ListenResponsePayload>): Promise<void> {
+	public async on<
+		EventNamespace extends string,
+		EventName extends string,
+		ListenPayload extends SchemaValues<
+			TestContract[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				TestContract[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		cb?: (
+			payload: ListenPayload
+		) => ListenResponsePayload | Promise<ListenResponsePayload>
+	): Promise<void> {}
 
-    }
-
-    public async emit<EventNamespace extends string, EventName extends string, EmitPayload extends SchemaValues<TestContract[EventNamespace][EventName]['emitPayload']>, ListenResponsePayload extends SchemaValues<ExtractListenerPayload<TestContract[EventNamespace][EventName]['listenResponsePayload']>>>(eventNamespace: EventNamespace, eventName: EventName, payload: EmitPayload, cb?: (payload: ListenResponsePayload) => void | Promise<void>): Promise<void> {
-
-    }
+	public async emit<
+		EventNamespace extends string,
+		EventName extends string,
+		EmitPayload extends SchemaValues<
+			TestContract[EventNamespace][EventName]['emitPayload']
+		>,
+		ListenResponsePayload extends SchemaValues<
+			ExtractListenerPayload<
+				TestContract[EventNamespace][EventName]['listenResponsePayload']
+			>
+		>
+	>(
+		eventNamespace: EventNamespace,
+		eventName: EventName,
+		payload: EmitPayload,
+		cb?: (payload: ListenResponsePayload) => void | Promise<void>
+	): Promise<void> {}
 }
