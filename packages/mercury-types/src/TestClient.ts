67de0816e3ce70af39e271b6c5088cdbb3f910cf
liquidg3 <me@taylorrome.ro>
2020-08-12 15:07:31 -0600

    reference fix

--- packages/mercury-types/src/TestClient.ts
+++ packages/mercury-types/src/TestClient.ts
@@ -4,7 +4,7 @@ import MercuryClient, {
 	MercuryAggregateResponse,
 	EventSignature,
 	EmitCallback,
-} from 'mercury.types'
+} from './mercury.types'
 
 export default class TestClient<Contract extends Record<string, any>>
 	implements MercuryClient<Contract> {
