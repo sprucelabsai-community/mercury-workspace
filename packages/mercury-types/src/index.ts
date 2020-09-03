731568ef833b258fec69a8c6a67c5d64e13ea633
liquidg3 <me@taylorrome.ro>
2020-09-03 16:01:58 -0600

    test

--- packages/mercury-types/src/index.ts
+++ packages/mercury-types/src/index.ts
@@ -1,1 +1,1 @@
-export { default as mercuryContractSchema } from '.spruce/schemas/local/v2020_09_01/mercuryContract.schema'
+export { default as mercuryContractSchema } from './.spruce/schemas/local/v2020_09_01/mercuryContract.schema'
