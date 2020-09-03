ebf6fc781b549e3f3a88c44c5f22b27fdb5c11a3
liquidg3 <me@taylorrome.ro>
2020-09-03 14:38:21 -0600

    aoeu

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = (api) => {
-	api.cache(true)
+	api.cache(false)
 
 	return {
 		ignore: ["**/testDirsAndFiles/**", "**/spruce-templates/src/templates/**"],
@@ -22,12 +22,6 @@ module.exports = (api) => {
 				{
 					root: ['./'],
 					alias: {
-						'#spruce/schemas/fields/fieldTypeEnum':
-							'./node_modules/@sprucelabs/schema/build/.spruce/schemas/fields/fieldTypeEnum',
-						'#spruce/schemas/fields/fieldClassMap':
-							'./node_modules/@sprucelabs/schema/build/.spruce/schemas/fields/fieldClassMap',
-						'#spruce/schemas/fields/field.types':
-							'./node_modules/@sprucelabs/schema/build/.spruce/schemas/fields/field.type',
 						'#spruce': './src/.spruce',
 					},
 				},
