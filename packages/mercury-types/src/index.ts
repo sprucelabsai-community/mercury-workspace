3ec0c561d36b2593bd2735b73f65497073fef5e9
liquidg3 <me@taylorrome.ro>
2020-09-04 09:19:54 -0600

    export types

--- packages/mercury-types/src/index.ts
+++ packages/mercury-types/src/index.ts
@@ -1,1 +1,3 @@
 export { default as mercuryContractSchema } from './.spruce/schemas/local/v2020_09_01/mercuryContract.schema'
+export * from './mercury.types'
+export { default as MercuryClient } from './mercury.types'
