040eb45317b77e87aeed68f9ad0212162a78774c
liquidg3 <me@taylorrome.ro>
2020-09-10 13:07:17 -0600

    progress

--- packages/mercury-types/src/index.ts
+++ packages/mercury-types/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export { default as mercuryContractSchema } from './.spruce/schemas/local/v2020_09_01/mercuryContract.schema'
 export * from './mercury.types'
 export { default as MercuryClient } from './mercury.types'
