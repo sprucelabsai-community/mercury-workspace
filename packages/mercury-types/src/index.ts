29e11331620863aefbe7bfa63a29a214d425c59a
liquidg3 <me@taylorrome.ro>
2020-09-14 15:55:05 -0600

    patch: constants exported

--- packages/mercury-types/src/index.ts
+++ packages/mercury-types/src/index.ts
@@ -3,3 +3,4 @@
 export { default as mercuryContractSchema } from './.spruce/schemas/mercuryTypes/v2020_09_01/mercuryContract.schema'
 export * from './mercury.types'
 export { default as MercuryClient } from './mercury.types'
+export * from './constants'
