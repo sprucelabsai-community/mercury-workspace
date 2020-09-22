934372ec7080889a5c98ddc2c442b739315dbef5
liquidg3 <me@taylorrome.ro>
2020-09-22 14:14:39 -0600

    patch: updated to merged namespace

--- packages/mercury-types/src/.spruce/schemas/fields/fieldClassMap.ts
+++ packages/mercury-types/src/.spruce/schemas/fields/fieldClassMap.ts
@@ -1,36 +1,6 @@
-
-import { AddressField } from '@sprucelabs/schema'
-import { BooleanField } from '@sprucelabs/schema'
-import { DateField } from '@sprucelabs/schema'
-import { DateTimeField } from '@sprucelabs/schema'
-import { DirectoryField } from '@sprucelabs/schema'
-import { DurationField } from '@sprucelabs/schema'
-import { FileField } from '@sprucelabs/schema'
-import { IdField } from '@sprucelabs/schema'
-import { NumberField } from '@sprucelabs/schema'
-import { PhoneField } from '@sprucelabs/schema'
-import { RawField } from '@sprucelabs/schema'
-import { SchemaField } from '@sprucelabs/schema'
-import { SelectField } from '@sprucelabs/schema'
-import { TextField } from '@sprucelabs/schema'
+import { fieldClassMap } from '@sprucelabs/schema'
 
 
-/** Value for looking up field classes by field type */
-const fieldClassMap = {
-		['address']: AddressField,
-		['boolean']: BooleanField,
-		['date']: DateField,
-		['dateTime']: DateTimeField,
-		['directory']: DirectoryField,
-		['duration']: DurationField,
-		['file']: FileField,
-		['id']: IdField,
-		['number']: NumberField,
-		['phone']: PhoneField,
-		['raw']: RawField,
-		['schema']: SchemaField,
-		['select']: SelectField,
-		['text']: TextField,
-} as const
+
 
 export default fieldClassMap
--- packages/mercury-types/src/.spruce/schemas/mercuryTypes/v2020_09_01/permissionContract.schema.ts
+++ packages/mercury-types/src/.spruce/schemas/mercuryTypes/v2020_09_01/permissionContract.schema.ts
@@ -1,6 +1,7 @@
 import { SpruceSchemas } from '../../schemas.types'
 
 
+
 import permissionSchema from '#spruce/schemas/mercuryTypes/v2020_09_01/permission.schema'
 
 const permissionContractSchema: SpruceSchemas.MercuryTypes.v2020_09_01.IPermissionContractSchema  = {
