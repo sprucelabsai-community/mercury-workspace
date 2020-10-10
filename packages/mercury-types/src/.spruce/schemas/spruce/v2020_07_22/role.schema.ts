f6753b8a3fed9deab5f73304db2560754121d7b7
liquidg3 <me@taylorrome.ro>
2020-10-10 07:06:33 -0600

    patch: core-schema updates

--- packages/mercury-types/src/.spruce/schemas/spruce/v2020_07_22/role.schema.ts
+++ packages/mercury-types/src/.spruce/schemas/spruce/v2020_07_22/role.schema.ts
@@ -8,7 +8,7 @@ const roleSchema: SpruceSchemas.Spruce.v2020_07_22.IRoleSchema  = {
 	version: 'v2020_07_22',
 	namespace: 'Spruce',
 	name: 'Role',
-	description: 'Everyone in Spruce breaks into 5 roles. Owner, District/Regional Manager, Manager, Teammate, and Guest.',
+	description: 'Every role in Spruce inherits from 5 bases. Owner, Group Manager, Manager, Teammate, and Guest.',
 	    fields: {
 	            /** Id. */
 	            'id': {
@@ -17,6 +17,20 @@ const roleSchema: SpruceSchemas.Spruce.v2020_07_22.IRoleSchema  = {
 	                isRequired: true,
 	                options: undefined
 	            },
+	            /** Name. */
+	            'name': {
+	                label: 'Name',
+	                type: 'text',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** Base. Used to determine the default permissions when this role is created and the fallback for when a permission is not set on this role. */
+	            'base': {
+	                label: 'Base',
+	                type: 'select',
+	                hint: 'Used to determine the default permissions when this role is created and the fallback for when a permission is not set on this role.',
+	                options: {choices: [{"label":"Owner","value":"owner"},{"label":"Group manager","value":"groupManager"},{"label":"Manager","value":"manager"},{"label":"Teammate","value":"teammate"},{"label":"Guest","value":"guest"},{"label":"Anonymous","value":"anonymous"}],}
+	            },
 	            /** Slug. */
 	            'slug': {
 	                label: 'Slug',
@@ -24,11 +38,10 @@ const roleSchema: SpruceSchemas.Spruce.v2020_07_22.IRoleSchema  = {
 	                isRequired: true,
 	                options: undefined
 	            },
-	            /** Name. */
-	            'name': {
-	                label: 'Name',
+	            /** Description. */
+	            'description': {
+	                label: 'Description',
 	                type: 'text',
-	                isRequired: true,
 	                options: undefined
 	            },
 	            /** . */
