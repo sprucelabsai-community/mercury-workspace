5997d7b29c70a278a977f7015d80a21438e4f2fd
liquidg3 <me@taylorrome.ro>
2020-10-13 14:47:22 -0600

    major: permission and event signature overhaul

--- packages/mercury-types/src/.spruce/schemas/mercuryTypes/v2020_09_01/permission.schema.ts
+++ packages/mercury-types/src/.spruce/schemas/mercuryTypes/v2020_09_01/permission.schema.ts
@@ -1,7 +1,8 @@
 import { SchemaRegistry } from '@sprucelabs/schema'
 import { SpruceSchemas } from '../../schemas.types'
 
-
+import defaultsByRoleSchema from '#spruce/schemas/mercuryTypes/v2020_09_01/defaultsByRole.schema'
+import statusFlagsSchema from '#spruce/schemas/mercuryTypes/v2020_09_01/statusFlags.schema'
 
 const permissionSchema: SpruceSchemas.MercuryTypes.v2020_09_01.IPermissionSchema  = {
 	id: 'permission',
@@ -17,48 +18,22 @@ const permissionSchema: SpruceSchemas.MercuryTypes.v2020_09_01.IPermissionSchema
 	                hint: 'Hyphen separated name for this permission, e.g. can-unlock-doors',
 	                options: undefined
 	            },
-	            /** Fallback to permission contract. If the person does not have a permission set (to them or their role), I will fallback to the permission contract defined here. Note: if a new permission is added to the contract, setting this to false will mean everybody fails checking for it. */
-	            'fallbackToPermissionContractIfPermissionNotSet': {
-	                label: 'Fallback to permission contract',
+	            /** Require all statuses. */
+	            'requireAllStatuses': {
+	                label: 'Require all statuses',
 	                type: 'boolean',
-	                hint: 'If the person does not have a permission set (to them or their role), I will fallback to the permission contract defined here. Note: if a new permission is added to the contract, setting this to false will mean everybody fails checking for it.',
-	                defaultValue: true,
+	                defaultValue: false,
 	                options: undefined
 	            },
-	            /** Match on. */
-	            'match': {
-	                label: 'Match on',
-	                type: 'select',
-	                defaultValue: "any",
-	                options: {choices: [{"label":"All","value":"all"},{"label":"Any","value":"any"}],}
+	            /** . */
+	            'defaultsByRoleBase': {
+	                type: 'schema',
+	                options: {schema: defaultsByRoleSchema,}
 	            },
-	            /** Clocked in. Is the person clocked in and ready to rock? */
-	            'clockedIn': {
-	                label: 'Clocked in',
-	                type: 'boolean',
-	                hint: 'Is the person clocked in and ready to rock?',
-	                options: undefined
-	            },
-	            /** Clocked out. When someone is not working (off the clock). */
-	            'clockedOut': {
-	                label: 'Clocked out',
-	                type: 'boolean',
-	                hint: 'When someone is not working (off the clock).',
-	                options: undefined
-	            },
-	            /** On premise. Are they at work (maybe working, maybe visiting). */
-	            'onPrem': {
-	                label: 'On premise',
-	                type: 'boolean',
-	                hint: 'Are they at work (maybe working, maybe visiting).',
-	                options: undefined
-	            },
-	            /** Off premise. They aren't at the office or shop. */
-	            'offPrem': {
-	                label: 'Off premise',
-	                type: 'boolean',
-	                hint: 'They aren\'t at the office or shop.',
-	                options: undefined
+	            /** . */
+	            'can': {
+	                type: 'schema',
+	                options: {schema: statusFlagsSchema,}
 	            },
 	    }
 }
