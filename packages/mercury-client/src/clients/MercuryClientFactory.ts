a9576d48f6fbd7035bc1fab099920dec30162004
Taylor Romero <me@taylorrome.ro>
2020-12-19 23:09:22 -0700

    minor: can mixin contracts to client

--- packages/mercury-client/src/clients/MercuryClientFactory.ts
+++ packages/mercury-client/src/clients/MercuryClientFactory.ts
@@ -1,20 +1,22 @@
 import { EventContract } from '@sprucelabs/mercury-types'
 import { eventContractUtil } from '@sprucelabs/spruce-event-utils'
-import { ConnectionOptions, MercuryClient } from './client.types'
-import { DEFAULT_HOST } from './constants'
-import SpruceError from './errors/SpruceError'
-import MercurySocketIoClient from './MercurySocketIoClient'
-import MutableContractClient from './tests/MutableContractClient'
+import { ConnectionOptions, MercuryClient } from '../client.types'
+import { DEFAULT_HOST } from '../constants'
+import SpruceError from '../errors/SpruceError'
+import MutableContractClient from './MutableContractClient'
+
+export type Client<Contract extends EventContract> = MercuryClient<Contract> & {
+	handlesEvent(eventNameWithOptionalNamespace: string): boolean
+	mixinContract(eventContract: EventContract): void
+}
 
 export default class MercuryClientFactory {
 	public static async Client<Contract extends EventContract>(
 		connectionOptions?: ConnectionOptions
-	): Promise<MercuryClient<Contract>> {
-		const {
-			host = DEFAULT_HOST,
-			contracts,
-			isTest = false,
-		} = connectionOptions || { contracts: [] }
+	): Promise<Client<Contract>> {
+		const { host = DEFAULT_HOST, contracts } = connectionOptions || {
+			contracts: [],
+		}
 
 		if (host.substr(0, 4) !== 'http') {
 			throw new SpruceError({ code: 'INVALID_PROTOCOL' })
@@ -31,10 +33,7 @@ export default class MercuryClientFactory {
 			contracts ?? []
 		)
 
-		const Client = isTest ? MutableContractClient : MercurySocketIoClient
-
-		//@ts-ignore
-		const client = new Client<Contract>({
+		const client = new MutableContractClient<Contract>({
 			host,
 			reconnection: false,
 			rejectUnauthorized: !connectionOptions?.allowSelfSignedCrt,
@@ -43,6 +42,6 @@ export default class MercuryClientFactory {
 
 		await client.connect()
 
-		return client as MercuryClient<Contract>
+		return client as Client<Contract>
 	}
 }
