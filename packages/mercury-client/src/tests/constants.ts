acbe36c7e036df2a7bfdccc357254039a2173d36
Tay <me@taylorrome.ro>
2021-08-03 13:52:01 -0600

    patch: fallback to demo phone if proxy phone not set

--- packages/mercury-client/src/tests/constants.ts
+++ packages/mercury-client/src/tests/constants.ts
@@ -6,4 +6,5 @@ export const DEMO_PHONE_REAUTH = process.env.DEMO_PHONE_REAUTH ?? '**MISSING**'
 export const DEMO_PHONE_GUEST = process.env.DEMO_PHONE_GUEST ?? '**MISSING**'
 export const DEMO_PHONE_TEAMMATE =
 	process.env.DEMO_PHONE_TEAMMATE ?? '**MISSING**'
-export const DEMO_PHONE_PROXY = process.env.DEMO_PHONE_PROXY ?? '**MISSING**'
+export const DEMO_PHONE_PROXY =
+	process.env.DEMO_PHONE_PROXY ?? DEMO_PHONE_TEAMMATE
