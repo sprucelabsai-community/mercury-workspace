4750320567e68f592e4e8b393aae9809d0331c28
Taylor Romero <me@taylorrome.ro>
2023-04-28 14:32:46 -0600

    patch: missed export

--- packages/mercury-client/src/index.ts
+++ packages/mercury-client/src/index.ts
@@ -1,3 +1,4 @@
 export { default as MercuryClientFactory } from './clients/MercuryClientFactory'
 export { default as MercuryTestClient } from './clients/MercuryTestClient'
 export * from './types/client.types'
+export { default as MercurySocketIoClient } from './clients/MercurySocketIoClient'
